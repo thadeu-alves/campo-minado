1915ff2f21b7e9e2d82cfa7653425e7955c54d50
brendomatias <brendonmatias.dev@gmail.com>
2026-01-29 13:51:23 -0300

    Implementa IFile com readFile e writeFile

--- src/campoMinado/File.java
+++ src/campoMinado/File.java
@@ -1,5 +1,5 @@
 package campoMinado;
-
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,12 +7,36 @@ public class File implements IFile  {
     @Override
     public Map<String, Integer> readFile(String path) {
         Map<String, Integer> scores = new HashMap<>();
-        //TODO Use a classe BufferedReader e FileReader para ler um arquivo txt
-        return Map.of();
+        try(BufferedReader ler = new BufferedReader(new FileReader(path))){
+        	String linha;
+            String[]partes;
+            while((linha = ler.readLine()) != null){
+            	partes = linha.split(":");
+            	String nome = partes[0];
+            	int pontuacao = Integer.parseInt(partes[1]);
+            	scores.put(nome,pontuacao);
+            }
+        }
+        catch(IOException e) {
+        	System.out.println("Erro ao ler o arquivo: " + e.getMessage());
+        }
+        return scores;
     }
 
     @Override
     public void writeFile(Map<String, Integer> score, String path) {
-        //TODO Use a classe BufferedReader para escrever em arquivos txt
+    	try(BufferedWriter escLinha = new BufferedWriter(new FileWriter(path))) {
+    		for(Map.Entry<String,Integer> linha: score.entrySet()) {
+    			String nome = linha.getKey();
+    			int pontuacao = linha.getValue();
+    			
+    			escLinha.write(nome + ":" + pontuacao);
+    			escLinha.newLine();
+    			
+    		}
+    	}
+    	catch (IOException e) {
+            System.out.println("Erro ao escrever no arquivo: " + e.getMessage());
+        }
     }
 }
